9eb7e12abff3fb67fde6aac01db6a6c092ab2958
Nicolas Gras <nicolas.gras@live.fr>
2019-12-02 18:05:06 +0100

    Create wordpress-starter-functions.php

---
+++ wordpress-starter-functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Wordpress Starter functions
+ * Plugin URI:        https://nicolas-gras.com
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.3
+ * Requires PHP:      7.2
+ * Author:            Nicolas Gras
+ * Author URI:        https://nicolas-gras.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wordpress-starter-functions
+ 
+ // Clean the Head
+ 
+ 
